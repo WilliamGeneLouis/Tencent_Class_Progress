cf5509096b30828964b81d938422292a5709bf44
WilliamGeneLouis <17041534@qq.com>
2022-01-15 19:07:31 +0800

    first commit

---
+++ prac_1.15.py
@@ -0,0 +1,9 @@
+'''
+Author: William
+Date: 2022-01-15 18:55:25
+LastEditTime: 2022-01-15 19:01:36
+LastEditors: Please set LastEditors
+Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+FilePath: /undefined/Users/guister.iu/Documents/课程学习进度/Untitled-1.py
+'''
+
