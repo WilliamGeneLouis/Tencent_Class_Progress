a27bfae150bdb9b23b0f475df9cd827ab52052ed
WilliamGeneLouis <17041534@qq.com>
2022-02-04 22:16:44 +0800

    记录部分字符串的函数使用例子

--- prac_01.py
+++ prac_01.py
@@ -1,10 +1,34 @@
-'''
+"""
 Author: William
 Date: 2022-01-15 18:55:25
-LastEditTime: 2022-01-18 20:37:18
+LastEditTime: 2022-02-04 10:43:31
 LastEditors: Please set LastEditors
-Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-FilePath: /undefined/Users/guister.iu/Documents/课程学习进度/Untitled-1.py
-'''
+Description: 打开GoroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+FilePath: /undefined/Users/gustier.iu/Documents/课程学习进度/Untitled-1.py
+"""
 
 print("hello world")
+
+print('whoami')
+
+s = ' i love python'
+
+print(s.split())  # 分割字符串
+
+print('x'.join(['i', 'love', 'python']))  # 添加字符在固定的区间
+
+print(s.strip())           # 去除句尾和句首的空格
+
+print(s.index('o'))           # 输出该字符首次出现的位置
+
+print(s.replace(' ', '#'))      # 用后边的字符代替文本中出现的所有需要替换的字符
+
+print(s.find('i'))              # 与index用法类似
+
+print(s.count('o'))             # 计算该字符在文本中出现的次数
+
+print(s.endswith('on'))         # 布尔类型的判断句末是否一致
+
+print(s.startswith(' '))        # 与end-with类似
+
+
--- readme.md
+++ readme.md
@@ -3,7 +3,7 @@
  * @Date: 2022-01-15 19:07:50
  * @LastEditTime: 2022-01-17 23:06:34
  * @LastEditors: Please set LastEditors
- * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @Description: 打开GoroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /课程学习进度/readme.md
 -->
 
