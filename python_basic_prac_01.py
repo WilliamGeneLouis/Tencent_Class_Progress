be9828172c7470ab19326a94e14ef275156655fc
WilliamGeneLouis <17041534@qq.com>
2022-02-06 10:41:29 +0800

    prac_01 finished

--- python_basic_prac_01.py
+++ python_basic_prac_01.py
@@ -1,51 +1,13 @@
-'''text = "gfmncwmsbgblrrpylqjyrcgrzwfylb.rfyrqufyramknsrcpqypcdmp.bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+text = "gfmncwmsbgblrrpylqjyrcgrzwfylb.rfyrqufyramknsrcpqypcdmp.bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
 
-index = 0
+print(ord('a'))
 
-while index < len(text):
+for element in text:
 
-    print(text[index], end='')
+	if element >= 'a' and element <= 'z':
+		element = chr ((ord(element) - ord('a') + 2 ) % 26 + ord('a'))
+		print(element, end='')
 
-    if text[index] == 'k':
-        text[index] == 'm'
-        index += 1
-
-    elif text[index] == 'o':
-        text[index] == 'q'
-        index += 1
-
-    elif text[index] == 'e':
-        text[index] == 'g'
-        index += 1
-
-    else:
-        index +=1
-
-else:
-    print('not found')
-
-
-b_list = list('asdasda')
-
-index = 0
-
-while index < len(b_list):
-
-    if b_list[index] == 'a':
-        print(index)
-
-        b_list[index] == 'g'            # 此时替换不起作用
-        print(b_list[index])
-
-        index +=1
-        print(index)
-
-    else:
-        print(b_list[index], end='')
-        index +=1
-
-else:
-    print('none')
-
-'''
+	else:
+		print(element, end='')
 
