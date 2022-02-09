ba701678e9e5c709df2bfe7b0a5e3f3b8ce7a84c
WilliamGeneLouis <17041534@qq.com>
2022-02-09 17:28:05 +0800

    添加python进阶内容至重写

--- Python_Advanced/Python_advanced_prac.py
+++ Python_Advanced/Python_advanced_prac.py
@@ -23,7 +23,7 @@ print(People.my_name)
 
 
 class Student(People):
-    # 添加People类到Student类中
+    # 添加People类到Student类中  Student类继承People类的所有函数
     def __init__(self, name, age, curriculum):
         super(Student, self).__init__(name, age)
         # 此时相当于在Student类直接使用People中的构造函数
@@ -32,6 +32,23 @@ class Student(People):
     def func2(self, opinion, time):
         return 'my opinion is : ' + opinion + time
 
+
+student = Student('william', 19, 'python advanced')
+print(student.curriculum)
+print(student.my_age)
+print(student.my_name)
+print(student.func2('spider man (Amazing By Andrew)', ' Play by Netflix'))  # 通过子类方法调用
+print(super(Student, student).func('Netflix'))  # 调用父类方法通过super
+
+class Teacher(People):
+    def __init__(self, name, age, classes):     # name和age属性必须要再次填写在该构造函数中（Python必须）
+        super(Teacher, self).__init__(name, age)
+        self.my_classes = classes
+
+class Foo(Student, People):
+
+    def __init__(self, name, age, classes, curriculum, opinion, time):
+        super(Foo, self).__init__(name, age, classes)    # 如果没有表清楚是从哪一个类里边调用参数，优先从填写的第一个类里边调用
 
 
 # print(student.opinion)  AttributeError: 'People' object has no attribute 'opinion'
